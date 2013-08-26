9532aadee384bed42623708e72c262db99a158ef
feng_z <feng_z@worksap.co.jp>
2013-08-26 18:51:21 +0800

    Connect UI and background logic.

---
+++ html/channel.js
@@ -0,0 +1,47 @@
+
+
+function Channel(channelId) {
+    this.ws = undefined;  // web socket object
+    this.channelId = channelId;
+    this.decryptor = undefined;
+    this.connector = undefined;
+
+    // bind events
+    var self = this;
+    document.querySelector('#' + this.channelId + ' > .channel-connect-btn').onclick = function() {
+        self.connector = document.querySelector('#' + self.channelId + ' > .channel-connector').value;
+        self.decryptor = document.querySelector('#' + self.channelId + ' > .channel-decryptor').value;
+        self.connect();
+    };
+};
+
+Channel.prototype.connect = function() {
+    if ('WebSocket' in window)  {
+        this.ws = new WebSocket('ws://' + this.connector);
+        var ws = this.ws;
+        ws.onopen = function() {
+            ws.send('Message to send');
+            console.log('Message is sent...');
+        };
+        ws.onmessage = function (evt) { 
+            console.log('Message is received...');
+            var data = evt.data;
+            console.log('received : ' + data);
+        };
+        ws.onclose = function() { 
+            console.log('Connection is closed...'); 
+        };
+    } else {
+        console.log('WebSocket NOT supported by your Browser!');
+    }
+
+};
+
+
+Channel.prototype.send = function(data) {
+};
+
+//<div id='sse'>
+//  <a href='javascript:WebSocketTest()'>Run WebSocket</a>
+//</div>
+
--- wschat-server.py
+++ wschat-server.py
@@ -3,14 +3,52 @@ import socket
 import websocket
 
 
+class ValidationError(Exception):
+    pass
+
+
+channels = {
+    'ch1': {
+        'key': 'piapiapia',
+        'users': ['user1', 'user2']}}
+
+
+def validate_handshake(frame):
+    l = frame.split('GET')[1].split('HTTP/1.1')[0].strip()[1:]
+    ch = l.split('/')[0]
+    user = l.split('/')[1]
+    global channels
+    print ch
+    print user
+    print channels
+    try:
+        if user in channels[ch]['users']:
+            return
+        else:
+            raise ValidationError
+    except KeyError:
+        raise ValidationError
+
+
+def push(msg):
+    pass
+
+
+def error(msg):
+    pass
+
+
 def worker_handler(conn, addr):
     try:
         frame = websocket.get_handshake_frame(conn, addr, 4096)
+        print frame
+        validate_handshake(frame)
         conn.send(websocket.accept_handshake(frame))
         frame = websocket.get_data_frame(conn, addr, 4096)
-        websocket.data_frame_info(frame)
         for i in range(2):
             conn.send(websocket.make_data_frame_reply(frame))
+    except ValidationError:
+        conn.close()
     finally:
         conn.close()
 
