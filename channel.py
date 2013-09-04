0ab720cab804a2021ad9c2db747b32a30b2156aa
feng_z <feng_z@worksap.co.jp>
2013-09-04 18:51:26 +0800

    Can chat. Need to rewrite front-end.

--- channel.py
+++ channel.py
@@ -14,7 +14,7 @@ class AccessDeniedError(Exception):
     pass
 
 
-def get_epoch_miliseconds():
+def get_epoch_milliseconds():
     return int(math.floor(time.time() * 1000))
 
 
@@ -23,62 +23,67 @@ class Channel(object):
     def __init__(self, encrypted_key=None):
         self.encrypted_key = encrypted_key
         self.pipes = {}  # dict of {conn: [addr, user, last-time]}
-        self.msgs = []   # list of (time, data)
+        self.msgs = []   # list of {'time': , 'user', 'text'}
 
-    def push_tunnel(self, conn):
+    def _add_msg(self, msg_entity):
+        m = msg_entity.copy()
+        m['time'] = get_epoch_milliseconds()
+        self.msgs.append(m)
+
+    def _push_tunnel(self, conn):
         try:
             while True:
                 last_time = self.pipes[conn][2]
-                for msg in filter(lambda m: m[0] > last_time, self.msgs):
-                    conn.send(websocket.reply(str(msg[0]) + msg[1]))
-                    self.pipes[conn][2] = msg[0]
+                for msg in filter(lambda m: m['time'] > last_time, self.msgs):
+                    conn.send(websocket.reply(json.dumps(msg)))
+                    self.pipes[conn][2] = msg['time']
         except (KeyError, socket.error):
             pass
         finally:
             self._close_tunnel(conn)
 
-    def recv_tunnel(self, conn):
+    def _recv_tunnel(self, conn):
         try:
+            user = self.pipes[conn][1]
             while True:
                 data = websocket.get_data_frame(conn)
                 if data[0] == '\x08' or data[0] == '\x88':
                     return
                 else:
                     data = websocket.payload_data(data)
-                    self.msgs.append((get_epoch_miliseconds(), data))
+                    self._add_msg({'user': user, 'text': data})
         except (KeyError, socket.error):
             pass
         finally:
             self._close_tunnel(conn)
 
+    def _close_tunnel(self, conn):
+        try:
+            user = self.pipes.pop(conn)[1]
+            self._add_msg({'user': 'system', 'text': str(user) + ' offline'})
+            conn.close()
+        except KeyError:
+            pass
+
     def accept(self, conn, addr, handshake_frame):
         l = handshake_frame.split('GET')[1].split('HTTP/1.1')[0].strip()[1:]
         user = l.split('/')[1]
-        if not user.startswith('user'):
-            raise AccessDeniedError
+        #if not user.startswith('user'):
+        #    raise AccessDeniedError
         try:
-            self.pipes[conn] = [addr, user, get_epoch_miliseconds()]
+            self.pipes[conn] = [addr, user, get_epoch_milliseconds()]
             conn.send(websocket.accept_handshake(handshake_frame))
 
-            self.msgs.append((get_epoch_miliseconds(), str(user) + ' online'))
+            self._add_msg({'user': 'system', 'text': str(user) + ' online'})
 
-            recv_thread = threading.Thread(target=self.recv_tunnel, args=(conn,))
+            recv_thread = threading.Thread(target=self._recv_tunnel, args=(conn,))
             recv_thread.daemon = True
             recv_thread.start()
 
-            push_thread = threading.Thread(target=self.push_tunnel, args=(conn,))
+            push_thread = threading.Thread(target=self._push_tunnel, args=(conn,))
             push_thread.daemon = True
             push_thread.start()
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             traceback.print_exception(exc_type, exc_value, exc_traceback, limit=10, file=sys.stdout)
             self._close_tunnel(conn)
-
-    def _close_tunnel(self, conn):
-        try:
-            user = self.pipes[conn][1]
-            self.msgs.append((get_epoch_miliseconds(), str(user) + ' offline'))
-            conn.close()
-            self.pipes.pop(conn)
-        except KeyError:
-            pass
--- html/channel.js
+++ html/channel.js
@@ -4,14 +4,17 @@ function Channel(channelId) {
     this.ws = undefined;  // web socket object
     this.channelId = channelId;
 
-    this.decryptorLabel = document.querySelector('#' + channelId + ' > .decryptor');
-    this.connectorLabel = document.querySelector('#' + channelId + ' > .connector');
-    this.statusLabel = document.querySelector('#' + channelId + ' > .status');
-    this.connectButton = document.querySelector('#' + channelId + ' > .connect-btn');
-    this.sendButton = document.querySelector('#' + channelId + ' > .send-btn');
+    this.decryptorLabel = document.querySelector('#' + channelId + ' .decryptor');
+    this.connectorLabel = document.querySelector('#' + channelId + ' .connector');
+    this.statusLabel = document.querySelector('#' + channelId + ' .status');
+    this.msgsDiv = document.querySelector('#' + channelId + ' .msgs');
+    this.connectButton = document.querySelector('#' + channelId + ' .connect-btn');
+    this.sendButton = document.querySelector('#' + channelId + ' .send-btn');
+    this.msgInput = document.querySelector('#' + channelId + ' .msg-input');
 
     // bind events
     var self = this;
+    //this.sendButton.disabled = true;
 
     // connect action
     this.connectButton.onclick = function() {
@@ -27,25 +30,38 @@ Channel.prototype.connect = function() {
     if (this.ws !== undefined) {
         this.ws.close();
     }
+
     if ('WebSocket' in window)  {
         this.ws = new WebSocket('ws://' + this.connectorLabel.value);
         var self = this;
         var ws = this.ws;
 
         ws.onopen = function() {
-            self.connectorLabel.disabled = true;
-            self.decryptorLabel.disabled = true;
-            self.statusLabel.innerHTML= 'connected';
+            //self.connectorLabel.disabled = true;
+            //self.decryptorLabel.disabled = true;
+            //self.connectButton.disabled = true;
+            //self.sendButton.disabled = false;
+            self.statusLabel.innerHTML = 'connected';
+            self.msgsDiv.innerHTML = '';
         };
 
         ws.onmessage = function (evt) { 
-            var data = evt.data;
-            console.log('received : ' + data);
+            var data = JSON.parse(evt.data);
+            var date = new Date(data.time);
+            self.msgsDiv.appendChild(document.createTextNode(date));
+            self.msgsDiv.appendChild(document.createElement('br'));
+            self.msgsDiv.appendChild(document.createTextNode(data.user));
+            self.msgsDiv.appendChild(document.createElement('br'));
+            self.msgsDiv.appendChild(document.createTextNode(data.text));
+            self.msgsDiv.appendChild(document.createElement('br'));
+            self.msgsDiv.appendChild(document.createElement('br'));
         };
 
         ws.onclose = function() { 
-            self.connectorLabel.disabled = false;
-            self.decryptorLabel.disabled = false;
+            //self.connectorLabel.disabled = false;
+            //self.decryptorLabel.disabled = false;
+            //self.connectButton.disabled = false;
+            //self.sendButton.disabled = true;
             self.statusLabel.innerHTML = 'closed';
         };
     } else {
@@ -55,9 +71,8 @@ Channel.prototype.connect = function() {
 
 
 Channel.prototype.send = function() {
+    console.log(this.msgInput.value);
+    this.ws.send(this.msgInput.value);
+    this.msgInput.value = '';
 };
-
-//<div id='sse'>
-//  <a href='javascript:WebSocketTest()'>Run WebSocket</a>
-//</div>
 
